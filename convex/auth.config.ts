7be5ca250c0ecaaf0271c65a4ddb46fec5d4c9f8
Rajdeep Mitra <rajdeepmitra8@gmail.com>
2025-05-25 13:25:59 +0530

    Initial commit

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://enabling-alien-47.clerk.accounts.dev/",
+      domain: "https://humble-racer-2.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ],
